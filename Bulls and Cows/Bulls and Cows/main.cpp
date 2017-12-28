71b4c141a3d9814761a55538c20aa15866d0bbfe
Seviran <schepens.arnout@gmail.com>
2017-12-29 00:42:37 +0100

    Removed all std::

--- Bulls and Cows/Bulls and Cows/main.cpp
+++ Bulls and Cows/Bulls and Cows/main.cpp
@@ -6,47 +6,59 @@ using namespace std;
 
 void PrintIntro();
 void PlayGame();
-string GetGuessAndPrintBack();
+string GetGuess();
+bool AskToPlayAgain();
 
 // The entry point for our application
 int main()
 {
-    PrintIntro();
+	PrintIntro();
 
-    PlayGame();
+	PlayGame();
 
-    // Exits the game
-    return 0;
+	AskToPlayAgain();
+
+	// Exits the application
+	return 0;
 }
 
 // Introduce the game
 void PrintIntro()
 {
-    constexpr int WORD_LENGTH = 5;
-    std::cout << "Welcome to Bulls and Cows, a fun word game\n";
-    std::cout << "Can you guess the " << WORD_LENGTH;
-    std::cout << " letter isogram I'm thinking off?\n";
-    std::cout << std::endl;
-    return;
+	constexpr int WORD_LENGTH = 5;
+	cout << "Welcome to Bulls and Cows, a fun word game\n";
+	cout << "Can you guess the " << WORD_LENGTH;
+	cout << " letter isogram I'm thinking off?\n";
+	cout << endl;
+	return;
 }
 
 void PlayGame()
 {
-    // Loop for the number of turns asking for guesses
-    constexpr int NUMBER_OF_TURNS = 5;
-    for (int count = 0; count < NUMBER_OF_TURNS; count++)
-    {
-        GetGuessAndPrintBack();
-        std::cout << std::endl;
-    }
+	// Loop for the number of turns asking for guesses
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 0; count < NUMBER_OF_TURNS; count++)
+	{
+		string Guess = GetGuess();
+		cout << endl;
+	}
 }
 
 // Get a guess from the player
-string GetGuessAndPrintBack()
+string GetGuess()
 {
-    std::string Guess = "";
-    std::cout << "Enter your guess: ";
-    std::getline(std::cin, Guess);
-    std::cout << "Your guess was: " << Guess << endl;
-    return Guess;
+	string Guess = "";
+	cout << "Enter your guess: ";
+	getline(cin, Guess);
+	return Guess;
+}
+
+bool AskToPlayAgain()
+{
+	cout << "Do you want to play again?";
+	string Response = "";
+	getline(cin, Response);
+	cout << endl;
+	char firstChar = Response[0];
+	return ((firstChar == 'y') || (firstChar == 'Y'));
 }
